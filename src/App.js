1abfe0012b769d711f74a84b099314f29a165f64
Catherine Dawe <dawe.cm@gmail.com>
2022-12-10 14:08:12 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const CityComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const WeatherComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      PRACTICE
+      <CityComponent>City </CityComponent>
+      <WeatherComponent>Weather</WeatherComponent>
+    </Container>
   );
 }
 
